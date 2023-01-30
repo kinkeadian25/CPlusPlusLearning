36112d1505c47110aefca8ef565aebce0012b1fe
= <kinkeadian@outlook.com>
2023-01-30 15:03:08 -0800

    first commit

---
+++ Section1/ProjectTwo/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num(100);
+    cout << "Hello from Project Two!" << endl;
+    cout << "Enter a number: ";
+    cin >> num;
+    cout << "You entered: " << num << endl;
+    return 0;
+}
---
+++ Section2/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "What is your favorite number?" << endl;
+    cin >> num;
+    cout << "Awesome, thats a great number! " << num << endl;
+    return 0;
+}
